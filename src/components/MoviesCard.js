5f7ce211d8700361164b61bc3eeaa33fc58982ae
Aziz <94492261+D-Aziz@users.noreply.github.com>
2021-12-11 16:45:15 +0100

    Add files via upload

---
+++ src/components/MoviesCard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactStars from "react-rating-stars-component"
+const MoviesCard = ({movie}) => {
+    return (
+        <div>
+            <div className="movie_card" id="tomb">
+  <div className="info_section">
+    <div className="movie_header">
+      <img className="locandina" src={movie.posterurl}/>
+      <h1>{movie.name}</h1>
+      <h4>2018, Roar Uthaug</h4>
+    </div>
+    
+    <div className="movie_desc">
+      <p className="text">
+       {movie.description}
+      </p>
+    </div>
+    <div className="rate">
+
+    <ReactStars
+    value={movie.rating}
+    edit={false}
+    count={5}
+    size={24}
+    activeColor="#ffd700"
+  />    </div>
+
+    
+   
+  </div>
+  <div className="blur_back tomb_back" style={{background:`url(${movie.backurl})`}}></div>
+</div>
+        </div>
+    )
+}
+
+export default MoviesCard
---
+++ src/components/MoviesList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MoviesCard from './MoviesCard'
+const MoviesList = ({movies,text}) => {
+    return (
+        <div>
+
+            <div className="list"> {movies.filter(el=>el.name.toLowerCase().includes(text.toLowerCase())).map((el)=> (
+            <MoviesCard movie={el}/>))}
+            </div>
+           
+        </div>
+    );
+};
+
+export default MoviesList;
